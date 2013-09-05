4199335728a6e0c65428d09c775d25aa2a628dfa
Christian Scholz <cs@comlounge.net>
2013-09-05 18:47:05 +0200

    start of better translation handling for login forms

--- camper/login.py
+++ camper/login.py
@@ -1,5 +1,11 @@
-from wtforms import Form, TextField, PasswordField, BooleanField, validators, SelectMultipleField, widgets                                                                               
-class UsernameLoginForm(Form):
+from wtforms import Form, TextField, PasswordField, BooleanField, validators, SelectMultipleField, widgets
+from userbase.handlers.forms import BaseForm
+
+class UsernameLoginForm(BaseForm):
+    """our own username login form which derives from userbase BaseForm which in turn derives
+    from the sf-babel BaseForm in case it is installed
+    """
+
     username    = TextField('Username', [validators.Length(max=200), validators.Required()])
     password    = PasswordField('Passwort', [validators.Length(max=135), validators.Required()])
     remember    = BooleanField('eingeloggt bleiben')
