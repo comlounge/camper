a6555fb8f533a9fad6575517cdbbce52fc5f2a7a
Christian Scholz <cs@comlounge.net>
2015-01-07 18:29:48 +0100

    added admin dashboard

---
+++ camper/barcamps/dashboard.py
@@ -0,0 +1,27 @@
+#encoding=utf8
+
+import copy
+import json
+from starflyer import Handler, redirect, asjson, AttributeMapper
+from camper import BaseForm, db, BaseHandler, is_admin, logged_in, ensure_barcamp
+from wtforms import *
+from sfext.babel import T
+from .base import BarcampBaseHandler
+import requests
+from camper import utils
+
+
+class DashboardView(BarcampBaseHandler):
+    """shows a simple dashboard"""
+
+    template = "dashboard.html"
+
+    @ensure_barcamp()
+    @logged_in()
+    @is_admin()
+    def get(self, slug = None):
+        """render the view"""
+        barcamp_id = self.barcamp._id
+        sessions = self.config.dbs.sessions.find({'barcamp_id' : str(barcamp_id)})
+        return self.render(sessioncount = sessions.count())
+
