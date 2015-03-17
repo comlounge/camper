2628a6fbc05b641efa9cfd9880f5862336583639
Christian Scholz <cs@comlounge.net>
2015-03-17 15:52:21 +0100

    properly reading json now and returning textual state as well (hopefully translated)

--- camper/blog/workflow.py
+++ camper/blog/workflow.py
@@ -12,12 +12,16 @@ class WorkflowView(BarcampBaseHandler):
     @ensure_barcamp()
     @is_admin()
     @asjson()
-    def post(self, slug = None, eid = None, state = None):
+    def post(self, slug = None, eid = None):
         """render the view"""
+        state = self.request.form.get("workflow", None)
         if state not in ('published', 'draft'):
             return {'status' : 'error', 'msg' : 'wrong workflow state given'}
         entry = self.config.dbs.blog.get(ObjectId(eid))
         entry.workflow = state
         entry.save()
-        return { 'status' : 'success', 'new_state' : entry.workflow }
+        return { 'status' : 'success', 
+                 'new_state' : entry.workflow,
+                 'new_text_state' : self._(entry.workflow.capitalize())
+                 }
 
