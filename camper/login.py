e1c007e277872f688db322219f5f8da991ce4a78
Christian Scholz <cs@comlounge.net>
2015-06-11 17:11:36 +0200

    added an email login form

--- camper/login.py
+++ camper/login.py
@@ -1,5 +1,7 @@
 from wtforms import Form, TextField, PasswordField, BooleanField, validators, SelectMultipleField, widgets
 from userbase.handlers.forms import BaseForm
+from sfext.babel import babel_module, T
+
 
 class UsernameLoginForm(BaseForm):
     """our own username login form which derives from userbase BaseForm which in turn derives
@@ -9,4 +11,13 @@ class UsernameLoginForm(BaseForm):
     username    = TextField('Username', [validators.Length(max=200), validators.Required()])
     password    = PasswordField('Passwort', [validators.Length(max=135), validators.Required()])
     remember    = BooleanField('eingeloggt bleiben')
+
+class EMailLoginForm(BaseForm):
+    """our own email login form which derives from userbase BaseForm which in turn derives
+    from the sf-babel BaseForm in case it is installed
+    """
+
+    email    	= TextField(T('E-Mail'), [validators.Length(max=100), validators.Required()])
+    password    = PasswordField(T('Password'), [validators.Length(max=135), validators.Required()])
+    remember    = BooleanField(T('remember me'))
 
