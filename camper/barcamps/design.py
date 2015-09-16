2d66e78b0cc2285ea77c44f2b297c28cc3299a8f
Christian Scholz <cs@comlounge.net>
2015-09-16 11:57:52 +0200

    removed MultiCheckbox field as we have it already in forms.py

--- camper/barcamps/design.py
+++ camper/barcamps/design.py
@@ -10,16 +10,6 @@ from .base import BarcampBaseHandler
 from camper import utils
 from camper.handlers.forms import *
 
-class MultiCheckboxField(SelectMultipleField):
-    """
-    A multiple-select, except displays a list of checkboxes.
-
-    Iterating the field will produce subfields, allowing custom rendering of
-    the enclosed checkbox fields.
-    """
-    widget = widgets.ListWidget(prefix_label=False)
-    option_widget = widgets.CheckboxInput()
-
 
 class DesignForm(BaseForm):
     """form for design aspects of a barcamp"""
