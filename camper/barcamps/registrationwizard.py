a60511e4f43dd2072124eb6e6ef502eb2b1b7f35
Christian Scholz <cs@comlounge.net>
2016-10-06 15:02:16 +0200

    logged in users can now register via the new registration wizard

--- camper/barcamps/registrationwizard.py
+++ camper/barcamps/registrationwizard.py
@@ -9,6 +9,18 @@ import xlwt
 from cStringIO import StringIO
 import datetime
 
+from camper.services import RegistrationService, RegistrationError
+
+class ProcessingError(Exception):
+    """marker expception for the process post method"""
+
+    def __init__(self, msg = ""):
+        self.msg = msg
+
+    def __repr__(self):
+        return "<ProcessingError: %s>" %self.msg
+
+
 class RegistrationWizard(BarcampBaseHandler):
     """one screen for all of the registration
 
@@ -61,20 +73,89 @@ class RegistrationWizard(BarcampBaseHandler):
         return form
 
 
+    def process_post_data(self):
+        """process all the incoming data. raises ProcessingError in case there is a failure.
+        This method should only be called on post"""
+
+        # unfortunately it's doubled here. 
+        regform = self.registration_form
+        userform = self.user_registration_form
+
+        if not self.logged_in and not userform.validate():
+            raise ProcessingError("user is not logged in and userform does not validate")
+
+        if not regform.validate():
+            raise ProcessingError("registration form does not validate")
+        
+        # do we have events? If not then this is not valid
+        eids = self.request.form.getlist("_bcevents")
+        if not eids:
+            raise ProcessingError("no events selected")
+
+
+        # do we have a new user?
+        new_user = not self.logged_in
+        if not self.logged_in:
+
+            # create new user and get the UID
+            print "we still need to create that user"
+            return "no user created yet"
+
+        else:
+            uid = unicode(self.user._id)
+
+
+        # store registration details
+        self.barcamp.registration_data[uid] = regform.data
+
+        # register for all the selected events
+        reg = RegistrationService(self, self.user)
+
+        for eid in eids:
+            event = self.barcamp.get_event(eid)
+            try:
+                reg.set_status(eid, "going")
+            except RegistrationError, e:
+                print "a registration error occurred", e
+                raise ProcessingError(str(e))
+                return 
+
+        self.barcamp.save()
+        
+        
+
 
     @ensure_barcamp()
     def get(self, slug = None):
         """show the complete registration form with registration data, user registration and more"""
 
+        
         regform = self.registration_form
+        userform = self.user_registration_form
+                
+        if self.request.method == "POST":
+            try:
+                self.process_post_data()
+            except ProcessingError, e:
+                self.flash(self._("Unfortunately an error occurred when trying to register you. Please try again or contact the barcamptools administrator."), category="danger")
+                print "error on processing post data", e
+            else:
+                if self.logged_in:
+                    self.flash(self._("You have been successfully registered. Please check your email."), category="success")
+                    return redirect(self.url_for(".user_events", slug = self.barcamp.slug))
+                else:
+                    raise "we still need a thank you page and hint for activation"
+                
 
         return self.render(
             view = self.barcamp_view,
             barcamp = self.barcamp,
             title = self.barcamp.name,
             form = regform,
-            userform = self.user_registration_form,
+            userform = userform,
             **self.barcamp)
+
+    post = get
 
 class EMailValidation(BaseHandler):
     """utility handler for checking it an email exists already"""
