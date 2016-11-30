46a88f7f578a7f71621406a44c5bbe02f6e5e0de
Christian Scholz <cs@comlounge.net>
2016-11-30 15:19:19 +0100

    after creating a barcamp you will now redirected to the new admin wizard to complete your barcamp

--- camper/barcamps/add.py
+++ camper/barcamps/add.py
@@ -147,7 +147,7 @@ class AddView(BaseHandler):
             barcamp = self.config.dbs.barcamps.put(barcamp)
 
             self.flash(self._("%s has been created") %f['name'], category="info")
-            return redirect(self.url_for("barcamps.edit", slug = barcamp.slug))
+            return redirect(self.url_for("barcamps.admin_wizard", slug = barcamp.slug))
         return self.render(form = form, slug = None, show_slug=True, bcid = '')
     post = get
 
