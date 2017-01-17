97fc7035f74afda1d3fdd601374b01e5993ec9d3
Christian Scholz <cs@comlounge.net>
2017-01-17 16:16:30 +0100

    fixed validators

--- camper/barcamps/edit.py
+++ camper/barcamps/edit.py
@@ -48,7 +48,7 @@ class BarcampEditForm(BaseForm):
     fbAdminId           = TextField(T("Facebook Admin-ID"), [validators.Length(max=100)], description=T("ID of the facebook admin for the facebook page for this barcamp if one exists"))
 
     contact_email       = TextField(T("Contact E-Mail"), [validators.Length(max=200), validators.Email()], description=T("an email address under which a barcamp admin can be contacted. This address will be publically displayed."))
-    imprint             = WYSIWYGField(T("Imprint"), [validators.Length(max=15)], description=T("Please describe in detail who is responsible for this barcamp. This is mandatory for paid barcamps."))
+    imprint             = WYSIWYGField(T("Imprint"), [validators.Length(max=2000)], description=T("Please describe in detail who is responsible for this barcamp. This is mandatory for paid barcamps."))
 
     location_name                = TextField(T("name of location"), [], description = T('please enter the name of the venue here'),)
     location_street              = TextField(T("street and number "), [], description = T('street and number of the venue'),)
