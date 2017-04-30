816468c344fa9616aed0a69224732fc7cbb2f23b
Christian Scholz <cs@comlounge.net>
2017-04-30 15:07:09 +0200

    support for sending newsletters to ticket owners

--- camper/barcamps/newsletter.py
+++ camper/barcamps/newsletter.py
@@ -21,15 +21,6 @@ class NewsletterForm(BaseForm):
     body    = TextAreaField(T("Newsletter body"), [validators.Required()],
                 #description = T('please describe your barcamp here'),
     )
-    recipients = MultiCheckboxField(T("Recipients"), [validators.Required()],
-        choices = [
-            #("subscribers", T("People watching the barcamp")), 
-            ("participants", T("Participants (going)")), 
-            ("maybe", T("People who might come (maybe)")), 
-            ("waitinglist", T("People on Waiting List"))
-        ],
-        default = ['participants', 'waitinglist']
-    )
 
     testmail = TextField(T("E-Mail address for testing the newsletter"),
                 description = T('put your own e-mail address here in order to send the newsletter to this address for testing purposes'),
@@ -46,6 +37,19 @@ class NewsletterEditView(BarcampBaseHandler):
     def get(self, slug = None):
         """render the view"""
         form = NewsletterForm(self.request.form, config = self.config, recipients = "all")
+        
+        # only add recipients for non ticket mode
+        # otherwise we send to all ticket owners
+        if not self.barcamp.ticketmode_enabled: 
+            form.recipients = MultiCheckboxField(T("Recipients"), [validators.Required()],
+                choices = [
+                    #("subscribers", T("People watching the barcamp")), 
+                    ("participants", T("Participants (going)")), 
+                    ("maybe", T("People who might come (maybe)")), 
+                    ("waitinglist", T("People on Waiting List"))
+                ],
+                default = ['participants', 'waitinglist']
+            )
         if self.request.method == 'POST' and form.validate():
             f = form.data
             mailer = self.app.module_map['mail']
@@ -66,23 +70,30 @@ class NewsletterEditView(BarcampBaseHandler):
                         )
             elif st=="live":
                 # send newsletter to recipients
-
                 recipient_ids = []
-                if "subscribers" in f['recipients']:
-                    recipient_ids = self.barcamp.subscribers
-                if "participants" in f['recipients']:
-                    # collect all the participants from all event
-                    for event in self.barcamp.eventlist:
-                        recipient_ids = recipient_ids + event.participants
-                if 'waitinglist' in f['recipients']:
-                    for event in self.barcamp.eventlist:
-                        recipient_ids = recipient_ids + event.waiting_list
-                if 'maybe' in f['recipients']:
-                    for event in self.barcamp.eventlist:
-                        recipient_ids = recipient_ids + event.maybe
+                
+                if self.barcamp.ticketmode_enabled:
+                    tickets = self.app.config.dbs.tickets
+                    # all the confirmed tickets
+                    tickets = tickets.get_tickets(barcamp_id = self.barcamp._id)
+                    recipient_ids = [t.user_id for t in tickets]
+                else:
+                    if "subscribers" in f['recipients']:
+                        recipient_ids = self.barcamp.subscribers
+                    if "participants" in f['recipients']:
+                        # collect all the participants from all event
+                        for event in self.barcamp.eventlist:
+                            recipient_ids = recipient_ids + event.participants
+                    if 'waitinglist' in f['recipients']:
+                        for event in self.barcamp.eventlist:
+                            recipient_ids = recipient_ids + event.waiting_list
+                    if 'maybe' in f['recipients']:
+                        for event in self.barcamp.eventlist:
+                            recipient_ids = recipient_ids + event.maybe
 
                 # unduplicate the list
                 recipient_ids = set(recipient_ids)
+                print "sending to", recipient_ids
                 users = self.app.module_map['userbase'].get_users_by_ids(recipient_ids)
                 for user in users:
                     send_to = user.email
