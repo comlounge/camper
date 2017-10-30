4223c8cd4b91bc2003a804f0085aaeeb109be363
Christian Scholz <mrtopf@gmail.com>
2017-10-30 15:42:37 +0100

    added pre-publication check
    esp with paid barcamps we now check for all the necessary legal information and show on each admin screen what is missing to publish it.

--- camper/barcamps/legaledit.py
+++ camper/barcamps/legaledit.py
@@ -17,9 +17,9 @@ class LegalEditForm(BaseForm):
     """legal edit form"""
 
     contact_email       = TextField(T("Contact E-Mail"), [validators.Length(max=200), validators.Email()], description=T("an email address under which a barcamp admin can be contacted. This address will be publically displayed."))
-    imprint             = WYSIWYGField(T("Imprint"), [validators.Required(), validators.Length(max=2000)], description=T("Please describe in detail who is responsible for this barcamp. This is mandatory for paid barcamps."))
-    tos                 = WYSIWYGField(T("Terms of Service"), [validators.Length(max=2000)], description=T("Please enter your terms of service here."))
-    cancel_policy       = WYSIWYGField(T("Cancellation Policy"), [validators.Length(max=2000)], description=T("Please describe your cancellation policy (make sure it complies to your local law)."))
+    imprint             = WYSIWYGField(T("Imprint"), [validators.Required(), validators.Length(min=100, max=20000)], description=T("Please describe in detail who is responsible for this barcamp. This is mandatory for paid barcamps."))
+    tos                 = WYSIWYGField(T("Terms of Service"), [validators.Length(min=100, max=20000)], description=T("Please enter your terms of service here."))
+    cancel_policy       = WYSIWYGField(T("Cancellation Policy"), [validators.Length(min=100, max=20000)], description=T("Please describe your cancellation policy (make sure it complies to your local law)."))
 
 
 class LegalEditView(BarcampBaseHandler):
@@ -45,12 +45,13 @@ class LegalEditView(BarcampBaseHandler):
             
             f = form.data
 
+
             # update it so we have the new data for comparison
             self.barcamp.update(f)
             self.barcamp.put()
             self.flash(self._("The barcamp has been updated."), category="info")
             return redirect(self.url_for("barcamps.legaledit", slug = self.barcamp.slug))
-
+        
         return self.render(form = form, bcid = str(self.barcamp._id))
     
     post = get
