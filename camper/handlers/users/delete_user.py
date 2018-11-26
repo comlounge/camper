312526eb8f4b50fa5da06f8eb46e97fb8203c53c
Christian Scholz <mrtopf@gmail.com>
2018-11-26 23:27:01 +0100

    added delete user handler for deleting your user and profile data/image

---
+++ camper/handlers/users/delete_user.py
@@ -0,0 +1,64 @@
+#encoding=utf8
+from starflyer import Handler, redirect, asjson
+from camper import BaseForm, db, BaseHandler
+from camper import logged_in, is_admin
+from wtforms import *
+from sfext.babel import T
+from camper.handlers.forms import *
+import werkzeug.exceptions
+from bson import ObjectId
+import uuid
+
+__all__ = ['DeleteView']
+
+class EMailForm(BaseForm):
+    """form for asking for email to confirm deletion"""
+    email         = TextField(T(u"E-Mail address"), [validators.Length(max=200), validators.Email(), validators.Required()])
+
+
+class DeleteView(BaseHandler):
+    """let a user delete itself"""
+
+    template = "users/delete_user.html"
+
+    @logged_in()
+    def get(self):
+        """render the view"""
+        form = EMailForm(self.request.form, config = self.config, app = self.app, handler = self)
+
+        if self.request.method=="POST":
+            if form.validate():
+                email = form.data['email']
+                if email == self.user.email:
+
+                    # delete user data
+                    self.user.email         = uuid.uuid4()
+                    self.user.bio           = ""
+                    self.user.username      = uuid.uuid4()
+                    self.user.twitter       = ""
+                    self.user.facebook      = ""
+                    self.user.homepage      = ""
+                    self.user.tshirt        = ""
+                    self.user.organisation  = ""
+
+                    # delete image
+                    image_id = self.user.image
+                    if image_id is not None:
+                        self.app.module_map.uploader.remove(image_id)
+
+                    #self.image              = None
+
+                    #self.user.save()
+                    self.flash(self._("Your user was successfully deleted"), category="info")
+
+                    # logout user
+                    self.app.module_map.userbase.logout(self)
+                    url = self.url_for('index')
+                    return redirect(url)
+                else:
+                    self.flash(self._("Your email was not correct"), category="danger")
+            else:
+                self.flash(self._("There have been errors in the form"), category="danger")
+        return self.render(form = form, user = self.user)
+
+    post = get
