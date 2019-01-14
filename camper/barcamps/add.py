26fdc260655c0fc73d85c906911fe31a94fa8555
Christian Scholz <mrtopf@gmail.com>
2019-01-14 13:51:54 +0100

    translations and some label fixes

--- camper/barcamps/add.py
+++ camper/barcamps/add.py
@@ -29,7 +29,7 @@ class BarcampAddForm(BaseForm):
     )
     hide_barcamp        = BooleanField(T('Hide Barcamp'), description=T(u'If enabled this will hide this barcamp from showing up in the front page and in search engines'))
     preregistration     = BooleanField(T('Enable Pre-Registration'), description=T(u'If enabled users can only pre-register and an admin needs to put them on the participation list manually. Please make sure you also change the waiting list mail template as this will be sent when a user pre-registers.'))
-    send_email_to_admins= BooleanField(T('Send email notifications'), description=T(u'If enabled barcamp administrators will receive notifications about people registering and unregistering from the barcamp'))
+    send_email_to_admins= BooleanField(T('Enable notifications'), description=T(u'If enabled barcamp administrators will receive email notifications for new registrations, cancellations and new session proposals'))
     hide_barcamp        = BooleanField(T('Hide Barcamp'), description=T(u'If enabled this will hide this barcamp from showing up in the front page and in search engines'))
     start_date          = MyDateField(u"Start-Datum", [], default=None, format="%d.%m.%Y")
     end_date            = MyDateField(u"End-Datum", [], default=None, format="%d.%m.%Y")
